e67af7148ac4e8afb7b9b783c0aa5e66343ab463
xp-zhao] <13688396271@163.com>
2019-07-11 21:59:34 +0800

    引入axios

--- src/components/page/Table.vue
+++ src/components/page/Table.vue
@@ -43,6 +43,11 @@ export default {
         }]
       }
     },
+
+    mounted () {
+        this.axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+            .then(response => (console.log(response)))
+    },
   components: {
 
   }
